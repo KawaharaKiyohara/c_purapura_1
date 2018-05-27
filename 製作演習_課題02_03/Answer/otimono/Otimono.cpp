85b3a20b13509ece420011f594cfa19c6d23f376
TakayukiKiyohara <nezumimusume30@gmail.com>
2018-05-28 00:28:23 +0900

    落ち物の課題に02と03を追加。

---
+++ 製作演習_課題02_03/Answer/otimono/Otimono.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "consoleLib.h"
+#include "Otimono.h"
+#include <algorithm>
+
+std::vector<Otimono>	otimonoArray;	//落ち物の可変長配列。
+int gameCount = 0;
+void OtimonoProgram()
+{
+	gameCount++;
+	if (gameCount == 10) {
+		//30フレーム経過したら新しい落ち物を発生させる。
+		Otimono otimono;
+		//落ち物のX座標をランダムで決定。
+		otimono.posX = (rand() % 14) + 1;
+		otimono.posY = 0;
+		//ゲームカウントを0に戻す。
+		gameCount = 0;
+		otimonoArray.push_back(otimono);
+	}
+	//全ての落ち物を落下させる。
+	for (auto& otimono : otimonoArray) {
+		if ((gameCount % 5) == 0){
+			//5フレームに一度落下する。
+			otimono.posY += 1;
+		}
+	}
+	//リストから削除。	
+	otimonoArray.erase(std::remove_if(otimonoArray.begin(), otimonoArray.end(), [&](auto& otimono)->bool{
+		return otimono.posY == 18 || otimono.dead == 1;
+	}), otimonoArray.end());
+
+	//描画。
+	for (auto& otimono : otimonoArray) {
+		kbcDrawMoji(otimono.posX, otimono.posY, '*');
+	}
+	
+}
---
+++ 製作演習_課題02_03/Answer/otimono/Source.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include "consoleLib.h"
+#include "otimono.h"
+#include <time.h>
+#include <stdlib.h>
+
+//プレイヤーの構造体
+struct SPlayer {
+	int posX;
+	int posY;
+	int score;
+};
+int main()
+{
+	//乱数を初期化。
+	srand((unsigned)time(NULL));
+
+	//２次元配列。教科書のp.256から配列に関する記述があります。多次元配列はp.269
+	int map[16][16] = {
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	//  0列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	//  1列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	//  2列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	//  3列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	//  4列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	//  5列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	//  6列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	//  7列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	//  8列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	//  9列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	// 10列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	// 11列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	// 12列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	// 13列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	// 14列目
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },	// 15列目
+		
+	};
+
+	//プレイヤー１。
+	SPlayer player;
+	player.posX = 3;
+	player.posY = 15;
+	player.score = 0;
+	//プレイヤー２。
+	SPlayer player2;
+	player2.posX = 12;
+	player2.posY = 15;
+	player2.score = 0;
+	
+	//ここからゲームループ。
+	while (true) {
+		//1フレームの開始。
+		kbcBeginFrame();
+		// ここから ///////////////////////////////////////////////////////////
+		{
+			//背景を描画。
+			//二次元配列のmapを参照して、背景を描画していく。
+			for (int y = 0; y < 16; y++) {
+				for (int x = 0; x < 16; x++) {
+					if (map[y][x] == 0) {
+						//2次元配列のmap[y][x]に0が入っていたら、
+						//座標x, yの場所に空白文字を描画する。
+						kbcDrawMoji(x, y, ' ');
+					}
+					else if (map[y][x] == 1) {
+						//2次元配列のmap[y][x]に1が入っていたら、
+						//座標x, yの場所に@を描画する。
+						kbcDrawMoji(x, y, '@');
+					}
+				}
+			}
+
+			//プレイヤー１の移動処理。
+			if (GetAsyncKeyState(VK_LEFT) != 0) {
+				//左のキーが押されたときの処理をここに記述する。
+				player.posX--;
+			}
+			if (GetAsyncKeyState(VK_RIGHT) != 0) {
+				player.posX++;
+			}
+			
+			//プレイヤー１の衝突判定。
+			if (player.posX < 1) {
+				player.posX = 1;
+			}
+			if (player.posX > 14) {
+				player.posX = 14;
+			}
+			//落ち物との衝突判定。
+			for (int i = 0; i < otimonoArray.size(); i++) {
+				if ( otimonoArray[i].posX == player.posX
+					&& otimonoArray[i].posY == player.posY
+				) {
+					otimonoArray[i].dead = 1;
+					player.score++;
+				}
+			}
+			//プレイヤーを描画する。
+			kbcDrawMoji(player.posX, player.posY, 'P');
+
+			//続いてプレイヤー２の移動処理。
+			if (GetAsyncKeyState('A') != 0) {
+				//左のキーが押されたときの処理をここに記述する。
+				player2.posX--;
+			}
+			if (GetAsyncKeyState('D') != 0) {
+				player2.posX++;
+			}
+
+			//player2の衝突判定。
+			if (player2.posX < 1) {
+				player2.posX = 1;
+			}
+			if (player2.posX > 14) {
+				player2.posX = 14;
+			}
+			//落ち物との衝突判定。
+			for (int i = 0; i < otimonoArray.size(); i++) {
+				if (otimonoArray[i].posX == player2.posX
+					&& otimonoArray[i].posY == player2.posY
+					) {
+					otimonoArray[i].dead = 1;
+					player2.score++;
+				}
+			}
+			//プレイヤーを描画する。
+			kbcDrawMoji(player2.posX, player2.posY, 'L');
+
+			//落ち物のプログラムを実行。
+			OtimonoProgram();
+
+			//スコアを表示する。
+			//文字列は教科書のp.284に記載があります。
+			//sprintf_sはマイクロソフトが独自に用意したもので、
+			//C言語の標準関数のsprintfをセキュア(安全)にしたものです。
+			//sprintfの挙動についてはググりましょう。
+			char scoreText[256];
+			sprintf_s(scoreText, "Player1　スコア %d", player.score);
+			kbcDrwaMojiRetu(0, 18, scoreText);
+
+			sprintf_s(scoreText, "Player2　スコア %d", player2.score);
+			kbcDrwaMojiRetu(0, 19, scoreText);
+
+			
+		}
+		// ここまでの間にゲームのプログラムを書いていってね//////////////////////
+		
+		//1フレームの終了。
+		kbcEndFrame();
+	}
+
+	return 0;
+}
